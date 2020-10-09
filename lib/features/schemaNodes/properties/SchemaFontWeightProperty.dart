eb2a80a78623ac8fa3bea83f5794d514ed6494c6
Evgeny Burdaev <hate2me@yandex.ru>
2020-10-09 15:33:47 +0300

    Serialize

--- lib/features/schemaNodes/properties/SchemaFontWeightProperty.dart
+++ lib/features/schemaNodes/properties/SchemaFontWeightProperty.dart
@@ -5,6 +5,14 @@ import 'package:flutter_app/features/schemaNodes/SchemaNodeProperty.dart';
 class SchemaFontWeightProperty extends SchemaNodeProperty {
   SchemaFontWeightProperty(String name, FontWeight value) : super(name, value);
 
+  Map<String, dynamic> toJson() {
+    return {
+      'propertyClass': 'SchemaFontWeightProperty',
+      'name': this.name,
+      'value': this.value.toString()
+    };
+  }
+
   @override
   SchemaFontWeightProperty copy() {
     return SchemaFontWeightProperty(this.name, value);
