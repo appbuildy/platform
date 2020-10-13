b5ce9ec0d90df231d22d00cde8520eea0d47b084
Evgeny Burdaev <hate2me@yandex.ru>
2020-10-13 21:03:16 +0300

    load project

--- lib/features/services/projects/LoadedProject.dart
+++ lib/features/services/projects/LoadedProject.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_app/features/schemaInteractions/Screens.dart';
+import 'package:flutter_app/features/schemaNodes/SchemaNode.dart';
+import 'package:flutter_app/features/services/project_load/ComponentLoadedFromJson.dart';
 import 'package:flutter_app/features/services/projects/IProjectLoad.dart';
 import 'package:flutter_app/store/schema/SchemaStore.dart';
 import 'package:flutter_app/store/schema/ScreensStore.dart';
@@ -10,11 +12,21 @@ class LoadedProject implements IProjectLoad {
 
   @override
   Screens load() {
-    final screen = SchemaStore(components: []);
+    final screen = SchemaStore(components: _loadComponents());
     final store = ScreensStore(screens: [screen]);
     final current = CurrentScreen(screen);
     final screens = Screens(store, current);
 
     return screens;
+  }
+
+  List<SchemaNode> _loadComponents() {
+    return jsonCanvas['screens']
+        .first['components']
+        .map((component) {
+          return ComponentLoadedFromJson(component).load();
+        })
+        .toList()
+        .cast<SchemaNode>();
   }
 }
