11b011aa52add3cc8317ec5601ae04f522a1ab3f
Evgeny Burdaev <hate2me@yandex.ru>
2020-10-28 21:25:48 +0300

    Deleted unused import

--- lib/utils/FontAwesomeIconsExtension.dart
+++ lib/utils/FontAwesomeIconsExtension.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 extension JsonSerializationIconData on IconData {
   static IconData fromJson(Map<String, dynamic> jsonValue) {
