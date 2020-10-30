77c5ad3a0f0e9e2c696d807a164e7c83de9523d1
Evgeny Burdaev <hate2me@yandex.ru>
2020-10-30 18:24:56 +0300

    OK

--- test/app_skeleton/screen_test.dart
+++ test/app_skeleton/screen_test.dart
@@ -5,15 +5,18 @@ import 'package:flutter_app/shared_widgets/widget_decorator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final btn =
-      WidgetDecorator(position: Offset(55, 55), widget: SharedWidget.button());
+  final btn = WidgetDecorator(
+      position: Offset(55, 55), widget: SharedWidget.button(text: 'test'));
   final app = MaterialApp(
       title: 'Test',
       home: Scaffold(
           body: Center(
         child: Screen(widgets: [btn]),
       )));
+
   testWidgets('it renders', (WidgetTester tester) async {
     await tester.pumpWidget(app);
+    final textFinder = find.text('test');
+    expect(textFinder, findsOneWidget);
   });
 }
