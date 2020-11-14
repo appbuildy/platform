f222baca04ab9ca28d222c856d60d4286cb3a56f
Evgeny Burdaev <hate2me@yandex.ru>
2020-11-14 17:03:58 +0300

    OK

--- lib/app_skeleton/loading/application_loaded_from_json.dart
+++ lib/app_skeleton/loading/application_loaded_from_json.dart
@@ -1,11 +1,23 @@
 import 'package:flutter_app/app_skeleton/application.dart';
+import 'package:flutter_app/app_skeleton/screen.dart';
 
 import 'i_application_load.dart';
 
 class ApplicationLoadedFromJson implements IApplicationLoad {
+  Map<String, dynamic> jsonApp;
+  ApplicationLoadedFromJson(this.jsonApp);
+
   @override
   Application load() {
-    // TODO: implement load
-    throw UnimplementedError();
+    return Application(screens: _loadScreens());
+  }
+
+  List<Screen> _loadScreens() {
+    return jsonApp['screens']
+        .map((screen) {
+          return Screen.fromJson(screen);
+        })
+        .toList()
+        .cast<Screen>();
   }
 }
