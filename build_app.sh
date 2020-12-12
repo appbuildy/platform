0ae8651974bf3afb77fdd261b0c4362cd3cf318f
Evgeny Burdaev <hate2me@yandex.ru>
2020-12-12 16:40:19 +0300

    Build

--- build_app.sh
+++ build_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PROJECT_URL=$1
-curl -XGET $PROJECT_URL > build_data.dart
+curl -XGET $PROJECT_URL > lib/build_data.dart
+flutter build ios -t lib/build_main.dart --no-tree-shake-icons 
