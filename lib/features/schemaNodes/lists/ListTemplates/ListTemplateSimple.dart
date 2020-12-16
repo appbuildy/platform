1110fb58595a332dd5bde70d0a5d3494e9442b14
Evgeny Burdaev <hate2me@yandex.ru>
2020-12-16 21:15:34 +0300

    Delete debug statements

--- lib/features/schemaNodes/lists/ListTemplates/ListTemplateSimple.dart
+++ lib/features/schemaNodes/lists/ListTemplates/ListTemplateSimple.dart
@@ -126,13 +126,6 @@ class ListTemplateSimple extends ListTemplate {
               .map((ListElementNode el) {
             Widget renderedWidget;
 
-            print('THEME 3');
-            print('THEME 3');
-            print('THEME 3');
-            print('THEME 3');
-            print(theme);
-            print('THEME 3');
-            print('THEME 3');
             if (el.node is DataContainer && el.columnRelation != null) {
               final String data =
                   item.value[el.columnRelation]?.data ?? 'no_data';
