8295ec241b8f4f49caed38e1cdbdca43fba45077
Evgeny Burdaev <hate2me@yandex.ru>
2021-01-13 21:37:25 +0300

    Load property for widget

--- lib/app_skeleton/data_layer/data_from_detailed_info.dart
+++ lib/app_skeleton/data_layer/data_from_detailed_info.dart
@@ -1,14 +1,25 @@
 import 'package:flutter_app/features/schemaNodes/SchemaNodeProperty.dart';
+import 'package:flutter_app/features/schemaNodes/properties/SchemaStringProperty.dart';
 import 'package:flutter_app/store/schema/DetailedInfo.dart';
 
 import 'i_element_data.dart';
 
 class DataFromDetailedInfo implements IElementData {
   DetailedInfo detailedInfo;
-  DataFromDetailedInfo(detailedInfo);
+  DataFromDetailedInfo(this.detailedInfo);
 
   @override
   SchemaNodeProperty getFor(Map<String, dynamic> key) {
     if (detailedInfo == null || key == null) return null;
+
+    return SchemaNodeProperty.deserializeFromJson({
+      "propertyClass": key['propertyClass'],
+      "name": _nameFor(key),
+      "value": detailedInfo.rowData[key['value']].data
+    });
+  }
+
+  _nameFor(key) {
+    return key['loadedPropertyName'] ?? 'Text';
   }
 }
